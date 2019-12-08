a5b5513f546119d3ddc08519ec9e090b2055de8b
N1ckky <nickkymistik@gmail.com>
2019-12-08 23:42:11 +0300

    upload
    Upload "first lever" exercises

---
+++ App.java
@@ -0,0 +1,13 @@
+package Nick_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
