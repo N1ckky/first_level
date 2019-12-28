e5e36616f7fb179727f7b8a2e1ebf79eafadd715
N1ckky <54807747+N1ckky@users.noreply.github.com>
2019-12-28 17:59:53 +0300

    corrections and clean code
    Some optimization, use principle DRY and made the code more readable

--- Nick_Maven/IO/MainTask.java
+++ Nick_Maven/IO/MainTask.java
@@ -1,18 +1,18 @@
 package Nick_Maven.IO;
 
-import java.io.IOException;
-
-import static Nick_Maven.IO.WorkWithFiles.folderOrTxtCheck;
+import static Nick_Maven.IO.WorkWithFiles.outputArray;
+import static Nick_Maven.IO.MyExceptions.IncorectPathException;
 
 public class MainTask {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IncorectPathException {
 //        for test
         String path = "src\\main\\java\\Nick_Maven\\IO\\TestFolder";
         String path2 = "src\\main\\java\\Nick_Maven\\IO\\HIM";
         String path3 = "src\\main\\java\\Nick_Maven\\IO\\TestFolder\\Main.txt";
         String path4 = "src\\main\\java\\Nick_Maven\\IO\\EmpFolder";
-        folderOrTxtCheck(path4);
+        String path5 = "src\\main\\java\\Nick_Maven\\IO\\arguments.png";
+        outputArray(path5);
 //        folderOrTxtCheck(args[0]);
     }
 }
--- Nick_Maven/IO/WorkWithFiles.java
+++ Nick_Maven/IO/WorkWithFiles.java
@@ -1,6 +1,10 @@
 package Nick_Maven.IO;
 
-import java.io.*;
+import Nick_Maven.IO.MyExceptions.IncorectPathException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -9,29 +13,83 @@ class WorkWithFiles {
     private static final String INDENT_DASH = "|-----";
     private static final String INDENT_SPACE = "|     ";
     private static final int TAB_LEVEL = 0;
+    private static boolean isTxt = false;
+    private static final String PATH_FOR_OUTPUT = "src\\main\\java\\Nick_Maven\\IO\\outputTree.txt";
+
+    private static int folderCounter = 1;
+    private static int fileCounter = 0;
+    private static int totalFilesNameLength = 0;
 
     private static List<String> arrayTree = new ArrayList<>();
 
-    static void outputArrayTree(File mainPath) {
+    static void outputArray(String path) throws IncorectPathException {
+        File mainPath = new File(path);
+        if (mainPath.exists() && mainPath.isDirectory()) {
+            arrayTree.add(path.substring(path.lastIndexOf('\\') + 1));
+        } else if (mainPath.exists() && mainPath.getName().contains("txt")) {
+            mainPath = new File(mainPath.getPath().substring(0, mainPath.getPath().lastIndexOf('\\') + 1));
+            isTxt = true;
+            System.out.println("You enter path to .TXT file \n---------------------------");
+        } else {
+            throw new IncorectPathException("You send must enter path to FOLDER or to .TXT FILE");
+        }
+
         File[] arr = mainPath.listFiles();
         readFilesTree(Objects.requireNonNull(arr), TAB_LEVEL);
-        if (arrayTree.size() > 1) {
-            if (arrayTree.get(1).length() == INDENT_SPACE.length())
-                arrayTree.remove(1);
 
-            File fileWanish = new File("src\\main\\java\\Nick_Maven\\IO\\outputTree.txt");
-            if (fileWanish.exists()) {
-                fileWanish.delete();
-            }
-            for (String s : arrayTree) {
-                System.out.println(s);
-                try (FileWriter writer = new FileWriter(fileWanish, true)) {
-                    writer.write(s + "\n");
-                } catch (IOException e) {
-                    e.printStackTrace();
+        if (!isTxt) {
+            if (arrayTree.size() > 1) {
+                if (arrayTree.get(1).length() == INDENT_SPACE.length())
+                    arrayTree.remove(1);
+
+                for (String s : arrayTree) {
+                    System.out.println(s);
                 }
+            } else System.out.println("The selected \"" + arrayTree.get(0) + "\" folder is Empty");
+        } else {
+            if (arrayTree.size() > 1) {
+                for (int i = 0; i < arrayTree.size(); i++) {
+                    String s = arrayTree.get(i);
+                    if (s.equals(INDENT_SPACE)) {
+                        arrayTree.remove(s);
+                    }
+                }
+                for (String s : arrayTree) {
+                    if (s.contains(INDENT_DASH)) {
+                        folderCounter += 1;
+                    }
+                    if (s.contains(INDENT_SPACE)) {
+                        fileCounter += 1;
+                        totalFilesNameLength += (s.length() - INDENT_SPACE.length());
+                    }
+                    System.out.println(s);
+                }
+
+                System.out.println();
+
+                if (fileCounter != 0)
+                    System.out.println("Average length of file names in this folder and subfolders: " + totalFilesNameLength / fileCounter);
+                System.out.println("Total folders found: " + folderCounter);
+                System.out.println("Total files found: " + fileCounter);
+                if (folderCounter != 0)
+                    System.out.println("Average number of files: " + fileCounter / folderCounter);
             }
-        } else System.out.println("The selected \"" + arrayTree.get(0) + "\" folder is Empty");
+        }
+        fileWanisher(PATH_FOR_OUTPUT);
+    }
+
+    private static void fileWanisher(String outputFile) {
+        File fileWanish = new File(outputFile);
+        if (fileWanish.exists()) {
+            fileWanish.delete();
+        }
+        for (String s : arrayTree) {
+            try (FileWriter writer = new FileWriter(fileWanish, true)) {
+                writer.write(s + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static void readFilesTree(File[] arr, int level) {
@@ -51,56 +109,6 @@ class WorkWithFiles {
                 }
                 readFilesTree(Objects.requireNonNull(f.listFiles()), level + 1);
             }
-        }
-    }
-
-    static void folderOrTxtCheck(String path) {
-        arrayTree.add(path.substring(path.lastIndexOf('\\') + 1));
-        File mainPath = new File(path);
-        if (mainPath.exists() && mainPath.isDirectory()) {
-            outputArrayTree(mainPath);
-        } else if (mainPath.exists() && mainPath.getName().contains("txt")) {
-            File mainTxt = new File(mainPath.getPath().substring(0, mainPath.getPath().lastIndexOf('\\') + 1));
-            outputArrayTxtFile(mainTxt);
-        }
-    }
-
-    static void outputArrayTxtFile(File mainTxt) {
-
-        int folderCounter = 1;
-        int fileCounter = 0;
-        int totalFilesNameLength = 0;
-
-        File[] arr = mainTxt.listFiles();
-        readFilesTree(arr, TAB_LEVEL);
-
-        if (arrayTree.size() > 1) {
-            arrayTree.remove(0);
-            for (int i = 0; i < arrayTree.size(); i++) {
-                String s = arrayTree.get(i);
-                if (s.equals(INDENT_SPACE)) {
-                    arrayTree.remove(s);
-                }
-            }
-            for (String s : arrayTree) {
-                if (s.contains(INDENT_DASH)) {
-                    folderCounter += 1;
-                }
-                if (s.contains(INDENT_SPACE)) {
-                    fileCounter += 1;
-                    totalFilesNameLength += (s.length() - INDENT_SPACE.length());
-                }
-                System.out.println(s);
-            }
-
-            System.out.println();
-
-            if (fileCounter != 0)
-                System.out.println("Average length of file names in this folder and subfolders: " + totalFilesNameLength / fileCounter);
-            System.out.println("Total folders found: " + folderCounter);
-            System.out.println("Total files found: " + fileCounter);
-            if (folderCounter != 0)
-                System.out.println("Average number of files: " + fileCounter / folderCounter);
         }
     }
 }
